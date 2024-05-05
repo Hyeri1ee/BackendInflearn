b9a2ff7fa1639888c783728a167300c922787475
Hyeri1ee <haerizian10@gmail.com>
2024-05-06 01:24:35 +0200

    update, delete api 추가

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -2,13 +2,11 @@ package com.group.libraryapp.controller.user;
 
 import com.group.libraryapp.domain.User;
 import com.group.libraryapp.dto.User.request.UserCreateRequest;
+import com.group.libraryapp.dto.User.request.UserUpdateRequest;
 import com.group.libraryapp.dto.User.response.UserResponse;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -25,21 +23,32 @@ public class UserController {
 
     @PostMapping("/user") //POST /user
     public void saveUser(@RequestBody UserCreateRequest request){
-        String sql = "INSERT INTO user (name, age) Values (?,?)";
+        String sql = "INSERT INTO user(name, age) Values (?,?)";
         jdbcTemplate.update(sql,request.getName(),request.getAge());
     }
 
-   @GetMapping("/user")
+    @GetMapping("/user")
     public List<UserResponse> getUsers(){
         String sql = "SELECT * FROM user";
-        return jdbcTemplate.query(sql, new RowMapper<UserResponse>() {
-            @Override
-            public UserResponse mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return jdbcTemplate.query(sql, (rs,rowNum) -> {
                 long id = rs.getLong("id");
                 String name = rs.getString("name");
                 int age = rs.getInt("age");
                 return new UserResponse(id,name, age);
-            }
+
         });
-   }
+    }
+
+    @PutMapping("/user")
+    public void updateUser(@RequestBody UserUpdateRequest request){
+        String sql = "UPDATE user SET name = ? WHERE id = ?";
+        jdbcTemplate.update(sql, request.getName(),request.getId());
+
+    }
+
+    @DeleteMapping("/user")
+    public void deleteUser(@RequestParam String name){
+        String sql = "DELETE from user WHERE name = ?";
+        jdbcTemplate.update(sql,name);
+    }
 }
