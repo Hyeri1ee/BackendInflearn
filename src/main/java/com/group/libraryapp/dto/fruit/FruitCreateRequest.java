0a3e92fa0e1100a1c9ceb6ff9713179224c7a16f
Hyeri1ee <haerizian10@gmail.com>
2024-05-11 01:07:01 +0200

    day6 과제-3단 분리(service, repository, controller)

--- src/main/java/com/group/libraryapp/dto/fruit/FruitCreateRequest.java
+++ src/main/java/com/group/libraryapp/dto/fruit/FruitCreateRequest.java
@@ -3,13 +3,21 @@ package com.group.libraryapp.dto.fruit;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
-@Component
 public class FruitCreateRequest {
 
     private String name;
     private LocalDate warehousingDate;
     private long price;
     private int saled;
+
+    public FruitCreateRequest() {
+    }
+
+    public FruitCreateRequest(String name, LocalDate warehousingDate, long price) {
+        this.name = name;
+        this.warehousingDate = warehousingDate;
+        this.price = price;
+    }
 
     public String getName() {
         return name;
