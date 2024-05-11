d1ab85e3fe400a637b5c8c9bce9c123a9db2c557
Hyeri1ee <haerizian10@gmail.com>
2024-05-12 01:52:04 +0200

    assign7: <1> fruit entity 생성

--- src/main/java/com/group/libraryapp/domain/Fruit.java
+++ src/main/java/com/group/libraryapp/domain/Fruit.java
@@ -1,13 +1,23 @@
 package com.group.libraryapp.domain;
 
+import javax.persistence.*;
 import java.time.LocalDate;
-
+@Entity
 public class Fruit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(nullable = false, length = 20, name = "name" )
     private String name;
+    @Column(nullable = false, name = "warehousingDate")
     private LocalDate warehousingDate;
+    @Column(nullable = false, name = "price")
     private long price;
+    @Column(nullable = false,name = "saled")
     private int saled;
+
+    public Fruit() {
+    }
 
     public Fruit(int id, String name, LocalDate warehousingDate, long price, int saled) {
         this.id = id;
--- src/main/java/com/group/libraryapp/repository/fruit/FruitRepository.java
+++ src/main/java/com/group/libraryapp/repository/fruit/FruitRepository.java
@@ -1,11 +1,12 @@
 package com.group.libraryapp.repository.fruit;
 
 import com.group.libraryapp.domain.Fruit;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
 import java.util.List;
 
-public interface FruitRepository {
+public interface FruitRepository extends JpaRepository<Fruit,Long> {
     public void saveFruit(String name, long price, LocalDate warehousingDate);
     public void saleFruit(int id);
     public List<Fruit> overviewFruit(String name);
