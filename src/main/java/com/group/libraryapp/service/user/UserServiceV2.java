aeeab45686aa9cb3966979061c9c6762dd79e38e
Hyeri1ee <haerizian10@gmail.com>
2024-05-14 12:01:41 +0200

    user @transactional 추가

--- src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
+++ src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
@@ -6,6 +6,7 @@ import com.group.libraryapp.dto.User.request.UserCreateRequest;
 import com.group.libraryapp.dto.User.request.UserUpdateRequest;
 import com.group.libraryapp.dto.User.response.UserResponse;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,24 +19,31 @@ public class UserServiceV2 {
         this.userRepository = userRepository;
     }
 
+    // 아래 있는 함수가 시작될 때 start transaction; 시작.
+    // 함수가 예외 없이 잘 끝났다면 commit
+    // 혹시라도 문제가 있다면 rollback
+    @Transactional
     public void saveUser(UserCreateRequest request){
         userRepository.save(new User(request.getName(),request.getAge()));
     }// INSERT SQL 이 자동으로 날라감.
 
+    @Transactional(readOnly = true)
     public List<UserResponse> getUser(){
         return userRepository.findAll().stream()
                 .map(UserResponse::new )
                 .collect(Collectors.toList());
     }
 
+    @Transactional
     public void updateUser(UserUpdateRequest request){
        User user =  userRepository.findById(request.getId())
                 .orElseThrow(IllegalArgumentException::new);
 
        user.updateName(request.getName());
-       userRepository.save(user);
+       //userRepository.save(user);
     }
 
+    @Transactional
     public void deleteUser(String name){
     User user = userRepository.findByName(name)
             .orElseThrow(IllegalArgumentException::new);
